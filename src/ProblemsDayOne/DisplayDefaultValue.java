d1d064c57bd2aebfd681a3569719f8233fdcca65
vik554 <kumbharvikram27@gmail.com>
2023-05-09 16:50:51 +0530

    added default value

---
+++ src/ProblemsDayOne/DisplayDefaultValue.java
@@ -0,0 +1,24 @@
+package ProblemsDayOne;
+
+public class DisplayDefaultValue {
+    public static void main(String[] args) {
+        byte defaultByte;      // 0
+        short defaultShort;     // 0
+        int defaultInt;         // 0
+        long defaultLong;       // 0L
+        float defaultFloat;     // 0.0f
+        double defaultDouble;   // 0.0d
+        char defaultChar;       // '\u0000'
+        boolean defaultBoolean; // false
+
+        System.out.println("Default values of primitive data types:");
+        //      System.out.println("byte: " + defaultByte);
+        //    System.out.println("short: " + defaultShort);
+        //  System.out.println("int: " + defaultInt);
+        //   System.out.println("long: " + defaultLong);
+        //      System.out.println("float: " + defaultFloat);
+        //    System.out.println("double: " + defaultDouble);
+        //   System.out.println("char: " + (int)defaultChar);
+        //    System.out.println("boolean: " + defaultBoolean);
+    }
+}
