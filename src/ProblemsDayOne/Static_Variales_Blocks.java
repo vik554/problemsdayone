f648213afc7fa9d616ea45a8a02be3533a3d2847
vik554 <kumbharvikram27@gmail.com>
2023-05-09 17:51:36 +0530

    added static variable

---
+++ src/ProblemsDayOne/Static_Variales_Blocks.java
@@ -0,0 +1,19 @@
+package ProblemsDayOne;
+
+public class Static_Variales_Blocks {
+    static int x = 10;
+    static int y;
+    static void func(int z)
+    {
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("z = " + z);
+    }
+    static {
+        System.out.println("Running static initialization block.");
+        y = x + 5;
+    }
+    public static void main(String args[]) {
+        func(8);
+    }
+}
